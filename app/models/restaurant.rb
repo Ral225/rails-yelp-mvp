e6759ee677686139392076e4da03e05a4da8dac6
Raheem Alibhai <ral@ivoireacademie.com>
2022-08-17 12:34:04 +0800

    models and tables dones

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
   CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, presence: true
-  validates :category, presence: true, inclusion: {in: CATEGORY}
-  validates :adress, presence: true
-  has_many :reviews, dependance: :destroy
+  validates :category, presence: true, inclusion: { in: CATEGORY }
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  has_many :reviews, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_034727) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_042342) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "category"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
